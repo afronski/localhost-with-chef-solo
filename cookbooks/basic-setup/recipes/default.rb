a543a9431daafad44a73cf62205c90951b29953f
Wojciech Gawroński <afronski@gmail.com>
2013-02-18 21:59:38 +0100

    Additional provisioned elements

--- cookbooks/basic-setup/recipes/default.rb
+++ cookbooks/basic-setup/recipes/default.rb
@@ -1,14 +1,49 @@
 # Important packages to install.
 vcss = %w{subversion git mercurial}
-packages = [ vcss ]
+
+languages = %w{}
+servers = %w{}
+
+opengl = %w{glfw glew mesa-demos glu freeglut}
+
+nosql = %w{}
+java = %w{}
+
+tools = %w{sublime-text}
+editors = %w{vim}
+
+browsers = %w{firefox chromium opera}
+
+fonts = %w{}
+office = %w{}
+
+packages = [
+    vcss,
+
+    opengl
+]
 
 packages.flatten.each do |a_package|
     package a_package
 end
 
+# AUR packages to install.
+aur_browsers = %w{google-chrome-dev firefox-nightly opera-next}
+
+aur_packages = [
+    aur_browsers
+]
+
+aur_packages.flatten.each do |a_aur_package|
+    pacman_aur a_aur_package
+end
+
 # All recipes to include.
 utils = %w{}
-recipes = [ ]
+
+recipes = [
+
+]
 
 recipes.flatten.each do |a_recipe|
     include_recipe a_recipe
