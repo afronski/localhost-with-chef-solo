cecdc9d1beaf7d18060943d5738f044b12f2ffd4
Wojciech Gawroński <afronski@gmail.com>
2013-02-19 18:55:45 +0100

    New approach - another recipes

--- cookbooks/pacaur/recipes/default.rb
+++ cookbooks/pacaur/recipes/default.rb
@@ -1,18 +1,17 @@
-packages = {
-    :cower => {
+packages = [
+    {
         :name => "cower",
         :version => "6-1-x86_64"
     },
-
-    :pacaur => {
+    {
         :name => "pacaur",
         :version => "3.2.3-1-any"
     }
-}
+]
 
 # Install program and dependencies for AUR packages and repositories management.
-packages.each do |pacaur_package|
-    package_name = "aur-packages/#{pacaur_package.name}-#{pacaur_package.version}.pkg.tar.xz"
+packages.each do |aur_management_package|
+    package_name = "aur-packages/#{aur_management_package.name}-#{aur_management_package.version}.pkg.tar.xz"
 
     execute "install prepared package for AUR repositories management (#{package_name})"
         command "pacaur -U --noedit --noconfirm #{package_name}"
