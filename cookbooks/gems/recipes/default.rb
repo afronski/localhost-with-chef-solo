204d1a065009c36f83ecbb94d79344ed5c14ee60
Wojciech Gawronski <afronski@gmail.com>
2013-02-20 23:23:28 +0100

    Small changes for gems

--- cookbooks/gems/recipes/default.rb
+++ cookbooks/gems/recipes/default.rb
@@ -1,8 +1,5 @@
 gems = %w{bundler rails foodcritic rake sass compass vagrant chef knife-ec2}
 
 gems.each do |gem|
-    gem_package gem do
-        owner "afronski"
-        group "users"
-    end
+    gem_package gem
 end
