1c66d64c66bcbece55abe45b3358b1e529d364a1
Wojciech Gawronski <afronski@gmail.com>
2013-02-20 23:42:41 +0100

    Additional gems

--- cookbooks/gems/recipes/default.rb
+++ cookbooks/gems/recipes/default.rb
@@ -1,4 +1,4 @@
-gems = %w{bundler rails foodcritic rake sass compass vagrant chef knife-ec2}
+gems = %w{bundler rails foodcritic rake sass compass vagrant chef knife-ec2 yajl-ruby erubis}
 
 gems.each do |gem|
     gem_package gem
