9e6b55763b3f106ae33fd36704f859f3f8c881a4
Wojciech Gawronski <afronski@gmail.com>
2013-04-11 09:43:11 +0200

    Adding new NPM package for node environment

--- cookbooks/install-node-environment/recipes/default.rb
+++ cookbooks/install-node-environment/recipes/default.rb
@@ -1,7 +1,12 @@
 include_recipe "npm"
 
 # Install global packages for node.
-global_packages = %w{bower express forever grunt-cli jscoverage jsgrep-toolkit jshint mocha plato requirejs weinre yeoman}
+global_packages = %w{bower express forever
+                     grunt-cli grunt-init
+                     jscoverage jsgrep-toolkit jshint
+                     mocha plato
+                     requirejs weinre
+                     yeoman}
 
 global_packages.each do |node_package|
     npm_package node_package
